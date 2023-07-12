072a8e769f3ae35d0865db03db43b767c7b3daa2
박광훈 <hyezoprk@kakao.com>
2023-07-12 13:16:46 +0900

    ✨ feat: iNIT 🐽

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<!-- <nav>
+	<li><a href="/card_flip">플립</a></li>
+	<li><a href="/marquee">마퀴</a></li>
+	<li><a href="/step_folder">폴더</a></li>
+</nav> -->
+<slot />
