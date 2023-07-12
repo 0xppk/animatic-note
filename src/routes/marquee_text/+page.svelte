07735438a333aa067993ce57e432a7a1d08a1ce9
박광훈 <hyezoprk@kakao.com>
2023-07-12 13:13:20 +0900

    ✨ feat: 인피니트 플로우 애니메이션

---
+++ src/routes/marquee_text/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import './index.css';
+	import { Marquee } from './model';
+
+	let marqueeP1: HTMLParagraphElement | null;
+	let marqueeP2: HTMLParagraphElement | null;
+
+	let marquee1: Marquee;
+	let marquee2: Marquee;
+
+	onMount(() => {
+		if (marqueeP1 && marqueeP2) {
+			marquee1 = new Marquee({
+				text: '기다려봐 가고 있는 중이야 이거 메시지가 좀 더 길어야지 초기화',
+				element: marqueeP1
+			});
+
+			marquee2 = new Marquee({
+				text: '기다려봐 가고 있는 중이야 이거 메시지가 좀 더 길어야지 초기화',
+				element: marqueeP2,
+				direction: 1
+			});
+		}
+
+		return () => {
+			marquee1.clearSetup();
+			marquee2.clearSetup();
+		};
+	});
+</script>
+
+<div class="container">
+	<div class="wrapper">
+		<p bind:this={marqueeP1} />
+	</div>
+
+	<div class="wrapper">
+		<p class="t" bind:this={marqueeP2} />
+	</div>
+</div>
---
+++ src/routes/marquee_text/model.ts
@@ -0,0 +1,76 @@
+import type { MarqueeEffect } from '$lib/abstract';
+
+type Options = {
+	text: string;
+	element: HTMLElement;
+	direction?: 1 | -1;
+};
+
+export class Marquee implements MarqueeEffect {
+	element: HTMLElement;
+	direction: 1 | -1;
+	distance: number;
+	velocity: number;
+	rafId: number;
+	scrollHandler: () => void;
+
+	constructor({ text, element, direction = -1 }: Options) {
+		this.element = element;
+		this.direction = direction;
+		this.distance = 0;
+		this.velocity = 3;
+
+		const textArray = text.split(' ');
+
+		// for 클리너
+		this.rafId = 0;
+		this.scrollHandler = () => {
+			this.distance += this.velocity * 2;
+		};
+
+		this.setup(textArray);
+		this.scrollSetup();
+		this.animate();
+	}
+
+	setup(textArray: string[]) {
+		// 2배로 늘려서 무한 스크롤링 효과를 준다.
+		textArray.push(...textArray);
+		textArray.forEach((text) => {
+			this.element.innerText += `${text}\u00A0\u00A0`;
+		});
+	}
+
+	scrollSetup() {
+		addEventListener('scroll', this.scrollHandler);
+	}
+
+	clearSetup() {
+		removeEventListener('scroll', this.scrollHandler);
+		cancelAnimationFrame(this.rafId);
+	}
+
+	flow() {
+		// 한바퀴 돌았으면 리셋하고
+		if (this.distance > this.element.scrollWidth / 2) {
+			this.element.style.transform = `translateX(0)`;
+			this.distance = 0;
+		}
+
+		this.distance += this.velocity;
+		this.element.style.transform = `translateX(${this.distance * this.direction}px)`;
+	}
+
+	animate() {
+		this.flow();
+
+		this.rafId = requestAnimationFrame(this.animate.bind(this));
+	}
+}
+
+// 잘못된 예. bind를 사용하면 removeEventListener가 안된다. 왜냐하면 bind를 사용하면 새로운 함수를 만들기 때문이다.
+// 그래서 컨스트럭터에서 this.scrollHandler를 걸어놓고 사용한다.
+// @see link{https://stackoverflow.com/questions/33859113/javascript-removeeventlistener-not-working-inside-a-class}
+// scrollSetup() {
+// 	addEventListener('scroll', this.scrollSetup.bind(this));
+// }
