bbc43f8ab05e9336044f5266cf7198a83b334a0b
박광훈 <hyezoprk@kakao.com>
2023-07-14 19:04:34 +0900

    ✨ feat: config 세분화

--- src/lib/abstract.ts
+++ src/lib/abstract.ts
@@ -1,15 +1,17 @@
-abstract class BaseEffect {
+export abstract class Effect {
 	abstract setup(...params: any): void;
 	abstract clearSetup(): void;
 	abstract animate(): void;
 }
 
-export interface ScrollEffect extends BaseEffect {
+export interface BaseEffect extends Effect {}
+
+export interface ScrollEffect extends Effect {
 	scrollSetup(): void;
 	resizeSetup(): void;
 }
 
-export interface MarqueeEffect extends BaseEffect {
+export interface MarqueeEffect extends Effect {
 	scrollSetup(): void;
 	flow(): void;
 }
---
+++ src/lib/config/index.ts
@@ -0,0 +1,16 @@
+export const piled_menu_showList = [
+	{ title: 'world', src: 'cover.webp' },
+	{ title: 'scenario', src: 'image.jpg' },
+	{ title: 'portfolio', src: 'cover.webp' },
+	{ title: 'crayon', src: 'image.png' },
+	{ title: 'project', src: 'image.png' }
+];
+
+export const random_shape_combination = [
+	{ config: 1, roundness: 1 },
+	{ config: 1, roundness: 2 },
+	{ config: 1, roundness: 4 },
+	{ config: 2, roundness: 2 },
+	{ config: 2, roundness: 3 },
+	{ config: 3, roundness: 3 }
+];
