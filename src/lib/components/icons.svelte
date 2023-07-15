f3b0f4ebf84ce2da002712ca40cf1fbbcb9166fd
박광훈 <hyezoprk@kakao.com>
2023-07-15 22:36:33 +0900

    ✨ feat: iconify 라이브러리 설치 및 컴퍼넌트 추가

---
+++ src/lib/components/icons.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+
+	export let src: keyof typeof list;
+	export let style: string = '';
+
+	const list = {
+		mango: 'twemoji:mango',
+		kimchi: 'twemoji:victory-hand-medium-light-skin-tone',
+		eyes: 'twemoji:eyes',
+		egg: 'tabler:egg-cracked',
+		zap: 'prime:bolt',
+		moon: 'mdi:moon-waning-crescent',
+		pac: 'iconoir:pacman',
+		vic: 'fluent-emoji:victory-hand-light',
+		peach: 'twemoji:peach',
+		candle: 'noto-v1:candle',
+		tongue: 'emojione:tongue'
+	};
+</script>
+
+<svelte:component this={Icon} icon={list[src]} class="icon" {style} />
