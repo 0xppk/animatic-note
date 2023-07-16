4d4278020fb188e05d9fbd1a990b0f92d2beb486
박광훈 <hyezoprk@kakao.com>
2023-07-16 23:57:41 +0900

    ✨ feat: marquee 이미지(이미지 플로우 애닌메이션)

---
+++ src/routes/marquee_image/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { MarqueeImage } from './model';
+	import { images } from '$lib/config';
+
+	let marqueeImage: HTMLDivElement | null;
+
+	onMount(() => {
+		new MarqueeImage({
+			element: marqueeImage,
+			images
+		});
+	});
+</script>
+
+<div class="container">
+	<div bind:this={marqueeImage} class="marquee-image" />
+</div>
+
+<style>
+	.container {
+		min-height: 100vh;
+		display: grid;
+		place-items: center;
+	}
+
+	.marquee-image {
+		height: 300px;
+		scale: 1;
+	}
+</style>
