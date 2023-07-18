0103a5615a3feb860abb6bb6c8e74af97ec96187
박광훈 <hyezoprk@kakao.com>
2023-07-18 15:55:47 +0900

    ✨ feat: 웨이브 섹션

---
+++ src/routes/wave_section/+page.svelte
@@ -0,0 +1,24 @@
+<div class="layout">
+	<div class="container" />
+</div>
+
+<style>
+	.layout {
+		background-color: #f5f3ea;
+		min-height: 100vh;
+	}
+	.container {
+		display: flex;
+		flex-direction: column;
+		margin: 2rem;
+		outline-offset: 0px;
+		background-color: #f96;
+		height: 50vh;
+		background-image: url('/svg/top_wave.svg'), url('/svg/bottom_wave.svg');
+		background-position: center bottom, center top;
+		background-repeat: no-repeat, no-repeat;
+		background-size: 150%, 150%;
+		outline: 3px #1b1c1d;
+		position: relative;
+	}
+</style>
